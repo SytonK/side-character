40e6a08221a61339ff0c73f9958ff0a9a30bd336
Sytonk <122539345+SytonK@users.noreply.github.com>
2024-08-08 19:15:54 +0300

    spacelines

--- player/states/ground/ground_state.gd
+++ player/states/ground/ground_state.gd
@@ -4,12 +4,12 @@ class_name GroundState extends PlayerState
 func physics_process(_delta: float) -> void:
 	_calc_animation()
 	_calc_state()
+	
 	player.move_and_slide()
 
 
 func _calc_animation() -> void:
-		player.animation_player.play('idle')
-
+	player.animation_player.play('idle')
 
 func _calc_state() -> void:
 	if !player.is_on_floor():
