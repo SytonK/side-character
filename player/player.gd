a8ba88479d2369df6d10ecf774b65a52bd5f4a8f
Sytonk <122539345+SytonK@users.noreply.github.com>
2024-08-09 20:35:15 +0300

    add friction

--- player/player.gd
+++ player/player.gd
@@ -6,7 +6,7 @@ class_name Player extends CharacterBody2D
 
 @export var max_speed: float = 400
 @export var acceleration: float = 800
-@export var friction: float
+@export var friction: float = 600
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
@@ -14,10 +14,17 @@ class_name Player extends CharacterBody2D
 func side_velocity(delta: float) -> void:
 	var side_input_strength = Input.get_action_strength('move_right') - Input.get_action_strength("move_left")
 	if side_input_strength == 0:
-		print('apply friction')
+		_apply_friction(delta)
 	else:
 		_apply_acceleration(side_input_strength, delta)
 
 func _apply_acceleration(side_input_strength: float, delta: float) -> void:
 	velocity.x = clamp(velocity.x + side_input_strength * acceleration * delta,
 		-max_speed, max_speed) 
+
+func _apply_friction(delta: float) -> void:
+	var x_velocity_length = abs(velocity.x)
+	if abs(velocity.x) > friction * delta:
+		velocity.x -= (velocity.x / x_velocity_length) * friction * delta
+	else:
+		velocity.x = 0
