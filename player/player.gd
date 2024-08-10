7cc6553b3f63dd07ce4d4b4c97c73ddb7175c273
Sytonk <122539345+SytonK@users.noreply.github.com>
2024-08-10 12:00:02 +0300

    add direction

--- player/player.gd
+++ player/player.gd
@@ -1,6 +1,9 @@
 class_name Player extends CharacterBody2D
 
 
+enum DIRECTION {LEFT, RIGHT}
+
+
 @export var max_fall_speed: float = 600
 @export var gravity_force: float = ProjectSettings.get_setting("physics/2d/default_gravity")
 
@@ -8,7 +11,11 @@ class_name Player extends CharacterBody2D
 @export var acceleration: float = 800
 @export var friction: float = 600
 
+
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var sprite_2d: Sprite2D = $Sprite2D
+
+var direction: DIRECTION = DIRECTION.RIGHT: set  = _on_change_direction
 
 
 func side_velocity(delta: float) -> void:
@@ -21,6 +28,7 @@ func side_velocity(delta: float) -> void:
 func _apply_acceleration(side_input_strength: float, delta: float) -> void:
 	velocity.x = clamp(velocity.x + side_input_strength * acceleration * delta,
 		-max_speed, max_speed) 
+	direction = DIRECTION.RIGHT if velocity.x > 0 else DIRECTION.LEFT
 
 func _apply_friction(delta: float) -> void:
 	var x_velocity_length = abs(velocity.x)
@@ -28,3 +36,8 @@ func _apply_friction(delta: float) -> void:
 		velocity.x -= (velocity.x / x_velocity_length) * friction * delta
 	else:
 		velocity.x = 0
+
+
+func _on_change_direction(new_directoin: DIRECTION) -> void:
+	direction = new_directoin
+	sprite_2d.flip_h = direction != DIRECTION.RIGHT
